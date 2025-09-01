0021a951b13bda86c6c5c547f37a11945a424197
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-09-01 18:24:34 +0500

    19. A Look Behind The Scenes Of Angular's Change Detection Mechanism

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -30,8 +30,37 @@ export class UserComponent {
     //========== 2. State change ========
     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
     this.selectedUser = DUMMY_USERS[randomIndex]
-
-
-    
   }
+
+   // NOTE
+    //================== zone.js ===================
+    /*
+      1. What is zone.jss? 
+      zone.js is a library that Angular uses to detect and track asynchronous operations like:
+
+      setTimeout
+      Promise
+      addEventListener
+      XHR / fetch calls
+
+      NOTE
+      Angular uses zone.js to automatically trigger change detection and update the UI.
+
+      Example:
+      When click is called the   zone.js tell to  anguler this is click event and do some changing 
+
+      NOTE
+      1. Does zone.js Update the UI Itself? ❌
+          No!
+          zone.js never updates the UI directly.
+          Its only job is to notify Angular whenever an asynchronous task or event happens.
+
+          After that, Angular itself runs change detection and updates the UI.
+      */
+     
+      name = "SAQIB"
+      changeName() {
+        const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+        this.name = 'Ahamd Ali : ' + randomIndex;
+      }
 }
