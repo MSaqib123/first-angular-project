d7579d852a65ee985b1ab753ccdd3f98628ba22a
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-09-04 23:19:37 +0500

    37. Outputting User-specific Tasks

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,21 +1,69 @@
-import { Component,Input } from '@angular/core';
-import { TaskComponent } from "./task/task.component";
+import { Component, Input } from '@angular/core';
+import { TaskComponent } from './task/task.component';
 
 @Component({
   selector: 'app-tasks',
   imports: [TaskComponent],
   templateUrl: './tasks.component.html',
-  styleUrl: './tasks.component.css'
+  styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
-  //================== Type script ===================
-  company:string = "";
-  select:string = "";
+  @Input() NameOfUser?: string;
+  @Input() id?:string;
+
+  tasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Master Angular',
+      summary:
+        'Learn all the basic and advanced features of Angular & how to apply them.',
+      dueDate: '2025-12-31',
+    },
+    {
+      id: 't2',
+      userId: 'u3',
+      title: 'Build first prototype',
+      summary: 'Build a first prototype of the online shop website',
+      dueDate: '2024-05-31',
+    },
+    {
+      id: 't3',
+      userId: 'u3',
+      title: 'Master issue template',
+      summary:
+        'Prepare and describe an issue template which will help with project management',
+      dueDate: '2024-06-15',
+    },
+    {
+      id: 't4',
+      userId: 'u2',
+      title: 'Prepare Master template',
+      summary:
+        'Prepare and describe an issue template which will help with project management',
+      dueDate: '2024-06-15',
+    },
+    {
+      id: 't5',
+      userId: 'u2',
+      title: 'Prepare Master template',
+      summary:
+        'Prepare and describe an issue template which will help with project management',
+      dueDate: '2024-06-15',
+    },
+    {
+      id: 't6',
+      userId: 'u4',
+      title: 'Prepare issue Master',
+      summary:
+        'Prepare and describe an issue template which will help with project management',
+      dueDate: '2024-06-15',
+    },
+  ];
+
+  get selectedUserTasks(){
+    console.log(this.tasks.filter((task)=>task.userId == this.id));
+    return  this.tasks.filter((task)=>task.userId == this.id);
+  }
   
-  
-  //================== Nullesh ===================
-  // @Input({required:true}) NameOfUser! :String; 
-  // @Input() NameOfUser? :String; 
-  // @Input() NameOfUser : String | undefined; 
-  @Input() NameOfUser? : string; 
 }
