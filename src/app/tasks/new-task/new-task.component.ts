a4993cfd4ce233425d1e5817f8428a599a1af0e9
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-09-05 09:56:13 +0500

    44. Using Directives & Two-Way-Binding

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,13 +1,36 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 
+//===================================
+//=========== Directives ============
+// Two Way binging using  FormModule Directives
+//===================================
+/*
+Example for 2 way binding
+  <p>
+    <label for="title">Title</label>
+    <input type="text" id="title" name="title" [(ngModel)] ="inputTitle" />
+  </p>
+  <p>
+    <label for="title">Title</label>
+    <input type="text" id="title" name="title" [(ngModel)] ="inputTitle" />
+  </p>
+*/
+
+import { FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-new-task',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
   @Output() hideTaskUI =new EventEmitter<void>();
+  
+  inputTitle = '';
+  inputSummery = '';
+  inputDate = '';
+
   hideTask(){
     this.hideTaskUI.emit();
   }
